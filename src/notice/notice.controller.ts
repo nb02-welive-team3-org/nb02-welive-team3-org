f2ae15638c86d5d3f8d5b4d6b353f0e92bd3deca
3023kk <3023kk@naver.com>
2025-11-03 13:53:05 +0900

    feat :  공지사항 알림 생성 기능 구현 함

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -4,14 +4,30 @@ import { NoticeListquerySchema } from './dto/list-notice.query.dto'
 import * as noticeService from './notice.service'
 import { UpdateRequestSchema } from './dto/update-notice.request.dto'
 import { DeleteNoticeRequestDto } from './dto/delete-notice.dto'
+import { createNotification } from './../notofications/notifications.service'
+import { NotificationType } from '../entities/notification.entity';
+import { residentListDetail } from '../residents/resident.service';
 
-//공지사항 CRUD 
 export const CreateNotice = async (req: Request, res: Response) => {
     const Result = CreateNoticeRequestSchema.parse({
         userId: (req as any).user?.id,
         ...req.body
     });
     await noticeService.createNotice(Result);
+
+    const residents = await residentListDetail(residentId, apartmentId);
+    const userIds = residents.map(resident => resident.userId);
+
+    console.log('입주민 수:', userIds.length); // 몇 명에게 알림을 보내는지 로그 기록
+
+    if (userIds.length > 0) {
+        await createNotification(
+            userIds,
+            `새로운 공지사항이 등록되었습니다.`,
+            NotificationType.SIGNUP_REQ
+        );
+    }
+
     return res.status(201).json({ message: '정상적으로 등록 처리되었습니다.' });
 };
 
